d141ed88bbd1ca517c5ad37601d7b9dbf9c13152
rahal13001 <rahal13001@mail.unpad.ac.id>
2022-07-03 02:03:50 +0900

    update binnary snappy

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
     
      'pdf' => [
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
+        'binary'  => 'vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64',
         'timeout' => false,
         'options' => [],
         'env'     => [],
@@ -43,7 +43,7 @@ return [
     
     'image' => [
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe"',
+        'binary'  => 'vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltoimage.amd64',
         'timeout' => false,
         'options' => [],
         'env'     => [],
